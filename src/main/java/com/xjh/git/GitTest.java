d5fcdaa2c42087cb9137c8e139b57446b1976c99
BearX <1270833305@qq.com>
2023-02-10 13:27:44 +0800

    hot-fix commit4

--- src/main/java/com/xjh/git/GitTest.java
+++ src/main/java/com/xjh/git/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("xjh");
         System.out.println("666");
+        System.out.println("666");
     }
 
 }
