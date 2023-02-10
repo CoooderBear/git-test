00746f96c39755e2d2be0b8158e36eb660b4e61c
BearX <1270833305@qq.com>
2023-02-10 13:49:27 +0800

    push commit

--- src/main/java/com/xjh/git/GitTest.java
+++ src/main/java/com/xjh/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("xjh");
         System.out.println("666");
         System.out.println("666");
+        System.out.println("push test");
     }
 
 }
